9ee7a5dd2c8c9ab131b1cb566e0c74ccb83901f0
Nick VIntartas <nv2009123@gmail.com>
2024-10-29 11:27:26 -0500

    Initial Commit

--- zombieRPGTest/ViewController.swift
+++ zombieRPGTest/ViewController.swift
@@ -9,6 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var level = 1
+    var XP = 0
+    var health = 100
+    var alive = true
+    
+    var baseDMG = 1
+    
+    @IBOutlet weak var levelLabel: UILabel!
+    @IBOutlet weak var XPLabel: UILabel!
+    @IBOutlet weak var HPLabel: UILabel!
+    
+    func update() {
+        if XP >= level * 10 {
+            level = level + 1
+            health = health + 10
+            XP = 0
+        }
+        if health <= 0 {
+            alive = false
+        }
+        if alive == false {
+            //write death code later
+        }
+        
+        levelLabel.text = "Level: \(level)"
+        XPLabel.text = "XP: \(XP)"
+        HPLabel.text = "Health: \(health)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

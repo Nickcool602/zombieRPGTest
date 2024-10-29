15eecf56daa1ab0c66ecce57e15f293845828f56
Nick VIntartas <nv2009123@gmail.com>
2024-10-29 10:53:50 -0500

    Initial Commit

---
+++ zombieRPGTestTests/zombieRPGTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  zombieRPGTestTests.swift
+//  zombieRPGTestTests
+//
+//  Created by NICHOLAS VINTARTAS on 10/29/24.
+//
+
+import Testing
+@testable import zombieRPGTest
+
+struct zombieRPGTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
